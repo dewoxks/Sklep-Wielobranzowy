4177466da60f94c56580937d7cbc4671c215a2c2
dewoxks <dewoxpl@gmail.com>
2025-12-28 23:54:31 +0100

    Implementacja systemu płatności, Dodanie klasy Zamowienie z obsługą statusów, Implementacja zgłoszeń zamówień

--- src/SklepWielobranzowy/model/ZgloszenieZamowienia.java
+++ src/SklepWielobranzowy/model/ZgloszenieZamowienia.java
@@ -1,4 +1,20 @@
 package SklepWielobranzowy.model;
 
+import java.time.LocalDate;
+import java.util.*;
+
 public class ZgloszenieZamowienia {
+    private Klient klient;
+    private List<String> numeryProduktow;
+    private LocalDate dataZgloszenia;
+
+    public ZgloszenieZamowienia(Klient klient, List<String> numeryProduktow) {
+        this.klient = klient;
+        this.numeryProduktow = new ArrayList<>(numeryProduktow);
+        this.dataZgloszenia = LocalDate.now();
+    }
+
+    public Klient getKlient() { return klient; }
+    public List<String> getNumeryProduktow() { return numeryProduktow; }
+    public LocalDate getDataZgloszenia() { return dataZgloszenia; }
 }
